6ebdb399fbc090f8b1af08aed21778c9fa9fdb7e
Sonu Ladia <ladiasonu@gmail.com>
2026-01-20 19:00:08 +0530

    intervals

---
+++ src/LeetCodeBlind75/MinNoOfArrows.java
@@ -0,0 +1,26 @@
+package LeetCodeBlind75;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class MinNoOfArrows {
+
+    public static void main(String[] args){
+        int[][] points = {{10,16},{2,8},{1,6},{7,12}};
+        System.out.println("The minimum number of arrows " +
+                "that must be shot to burst all balloons: " + findMinArrowShots(points));
+    }
+
+    public static int findMinArrowShots(int[][] points) {
+        Arrays.sort(points, Comparator.comparingInt(a -> a[1]));
+        int arrows = 1;
+        int prevEnd = points[0][1];
+        for(int i=1; i<points.length; i++){
+            if(points[i][0] > prevEnd){
+                arrows++;
+                prevEnd = points[i][1];
+            }
+        }
+        return arrows;
+    }
+}
---
+++ src/LeetCodeBlind75/NonOverlappingIntervals.java
@@ -0,0 +1,25 @@
+package LeetCodeBlind75;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class NonOverlappingIntervals {
+    public static void main(String[] args){
+        int[][] intervals = {{1,2}, {2,3}, {3,4}, {1,3}};
+        System.out.println("The minimum number of intervals you need to " +
+                "remove to make the rest of the intervals non-overlapping: " + eraseOverlapIntervals(intervals));
+    }
+
+    public static int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[1]));
+        int count = 1;
+        int prevEnd = intervals[0][1];
+        for(int i=1; i<intervals.length; i++){
+            if(intervals[i][0] >= prevEnd){
+                count++;
+                prevEnd = intervals[i][1];
+            }
+        }
+        return intervals.length - count;
+    }
+}
